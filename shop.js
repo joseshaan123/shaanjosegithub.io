295cc6f97fb513dd3b07d9065030505b9d92cb98
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2024-06-02 12:44:39 +0400

    shaanjoe.github.io

---
+++ shop.js
@@ -0,0 +1,6 @@
+let addtocart=0;
+function addCart(){
+    addtocart++;
+    document.getElementById('addtocart').textContent='Add to Cart:'+addtocart;
+    document.getElementById('addtocart2').textContent='Added to Cart:'+addtocart;
+}
